42c34988d813230c6c99e952cc1fabbb90f3dfdd
bpoiko <epicb35@gmail.com>
2025-09-27 16:01:43 -0700

    Longest Consecutive Sequence!

---
+++ longest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class longest{
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numba = new HashSet<>();
+
+        for(int num : nums){
+            numba.add(num);
+        }
+
+        int longest = 0;
+        for(int n : numba){
+            if(!numba.contains(n-1)){
+                int length = 1;
+                while(numba.contains(n + length)){
+                    length++;
+                }
+                longest = Math.max(longest,length);
+            }
+        }
+        return longest;
+    }
+
+}
