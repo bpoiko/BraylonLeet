92d03e800b4b6dbc7d701f8a75af84655bb141d5
bpoiko <epicb35@gmail.com>
2026-02-02 11:09:40 -0800

    minwindowOhyeah

--- minwin.java
+++ minwin.java
@@ -1,43 +1,40 @@
 import java.util.*;
 class minwin{
     public String minWindow(String s, String t) {
-        if(s.equals(t)){
-            return s;
+        if(t.isEmpty()) return "";
+
+        Map<Character, Integer> tCount = new HashMap<>();
+        Map<Character,Integer> sNeed = new HashMap<>();
+
+        for(char c : t.toCharArray()){
+            tCount.put(c, tCount.getOrDefault(c,0) + 1);
         }
-        if(t.length() > s.length()){
-            return "";
-        }
-        //build char frequency 
-        int required = 0;
-        Map<Character, Integer> charFreq = new HashMap<>();
-        for(char c :  t.toCharArray()){
-            charFreq.put(c,charFreq.getOrDefault(c,0) + 1);
-            required++;
-        }
-        
-        
-        int left = 0;
-        int right = 1;
-        Map<Character,Integer> secFreq = new HashMap<>();
-        while(right < s.length()){
-            while(left < right){
-                
-                if(charFreq.getOrDefault(s.charAt(right),0) >= 1){
-                //nice
-                secFreq.put(s.charAt(right),secFreq.getOrDefault(s.charAt(right), 0)+1);
-                
+        int resLen = Integer.MAX_VALUE;
+        int[] res = {-1,-1};
+        int have = 0, need = tCount.size();
+        int l = 0;
+        for(int r = 0; r < s.length(); r++){
+            char c = s.charAt(r);
+            sNeed.put(c,sNeed.getOrDefault(c,0) + 1);
+
+            if(tCount.containsKey(c) && tCount.get(c).equals(sNeed.get(c))){
+                have++;
             }
-                 right++;
-            }
-            //left really shouldnt move until right bumps into a match
-            //
-          
+            while(have == need){
+                if((r - l + 1) < resLen){
+                    resLen = r - l + 1; // window size
+                    res[0] = l;
+                    res[1] = r;
+                }
 
-
+                char leftChar = s.charAt(l);
+                sNeed.put(leftChar,sNeed.get(leftChar) - 1); // tighten the window
+                if(tCount.containsKey(leftChar) &&  sNeed.get(leftChar) < tCount.get(leftChar)){
+                    have--;
+                }
+                l++;
+            } // shrink your window
         }
-        // once all characters found exit and return string..
-        
-
-      return s.substring(left,right);
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
     }
 }
